7b69fc43c07b7a25fd516388979e6c60b60fd4c2
oscaroceguera <oscaroceguera@gmail.com>
2017-06-26 11:50:13 -0500

    Authenticate state

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,12 +3,34 @@ import { graphql } from 'react-apollo'
 import query from '../queries/CurrentUser'
 
 class Header extends React.Component {
+  renderButtons () {
+
+    const { loading, user } = this.props.data
+
+    if (loading) {
+      return <div />
+    }
+
+    if (user) {
+      return <div>Logout</div>
+    } else {
+      return (
+        <div>
+          You're not signed in.
+        </div>
+      )
+    }
+  }
+
   render () {
     console.log('Query', this.props.data);
+
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          {this.renderButtons()}
+        </div>
+      </nav>
     )
   }
 }
