97e1760617c040d6588403a8cb1a649a99892022
oscaroceguera <oscaroceguera@gmail.com>
2017-06-27 17:13:54 -0500

    Fix Signup Too

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -3,6 +3,7 @@ import AuthForm from './AuthForm'
 import LoginMutation from '../mutations/Login'
 import { graphql } from 'react-apollo'
 import query from '../queries/CurrentUser'
+import { hashHistory } from 'react-router'
 
 class LoginForm extends React.Component {
   constructor (props) {
@@ -11,19 +12,25 @@ class LoginForm extends React.Component {
       errors: []
     }
   }
+  componentWillUpdate (nextProps) {
+    if (!this.props.data.user && nextProps.data.user) {
+      // redirect to dashboard!!!
+      hashHistory.push('/dashboard')
+    }
+  }
   onSubmit ({ email, password }) {
     this.props.mutate({
       variables: { email, password },
       refetchQueries: [{ query }]
     }).catch(res => {
-      const errors = res.graphQLErrors.map( error => error.message)
+      const errors = res.graphQLErrors.map(error => error.message)
       this.setState({ errors })
     })
   }
   render () {
     return (
       <div>
-        <h3>Login</h3>
+        <h3>{`Login`}</h3>
         <AuthForm
           onSubmit={this.onSubmit.bind(this)}
           errors={this.state.errors}
@@ -33,6 +40,6 @@ class LoginForm extends React.Component {
   }
 }
 
-export default graphql(LoginMutation)(
-  graphql(query)(LoginForm)
+export default graphql(query)(
+  graphql(LoginMutation)(LoginForm)
 )
--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -3,6 +3,7 @@ import AuthForm from './AuthForm'
 import { graphql } from 'react-apollo'
 import mutation from '../mutations/Signup'
 import query from '../queries/CurrentUser'
+import { hashHistory } from 'react-router'
 
 class SignupForm extends React.Component {
   constructor (props) {
@@ -11,11 +12,17 @@ class SignupForm extends React.Component {
       errors: []
     }
   }
+  componentWillUpdate (nextProps) {
+    if (nextProps.data.user && !this.props.data.user) {
+      hashHistory.push('/dashboard')
+    }
+  }
   onSubmit ({ email, password }) {
     this.props.mutate({
       variables: { email, password },
       refetchQueries: [{ query }]
-    }).catch(res => {
+    })
+    .catch(res => {
       const errors = res.graphQLErrors.map(error => error.message)
       this.setState({ errors })
     })
@@ -33,4 +40,6 @@ class SignupForm extends React.Component {
   }
 }
 
-export default graphql(mutation)(SignupForm)
+export default graphql(query)(
+  graphql(mutation)(SignupForm)
+)
